57d493c8ec4fed35ef3ade8825dfbf2128b1607f
Dan Wooster <60037980+dwoosterup@users.noreply.github.com>
2021-01-30 09:12:29 -0500

    Update chimp.py

--- chimp.py
+++ chimp.py
@@ -1,35 +1,3 @@
-"""
-This simple example is used for the line-by-line tutorial
-that comes with pygame. It is based on a 'popular' web banner.
-Note there are comments here, but for the full explanation,
-follow along in the tutorial.
-
-NOTE: run the program and observe the errors. Figure out where and why these occur. This program opens several external files. You will need to locate and download these files before it can run. 
-
-Here's a great reference to help you learn the code behind Chimp - https://www.pygame.org/docs/tut/ChimpLineByLine.html
-"""
-"""
-- make screen bigger (5)
-- make monkey move around the outter edges of the window (10)
-- keep score (hits and misses) and display on the screen cleanly and clearly - your design (10)
-- allow user to pause the game (5)
-- give user menu option to select various levels (10)
-    - easy - like it is
-    - medium - monkey moves around the edges
-    - harder - monkey moves randomly around the edges (5)
-    - hardest - monkey moves randomly in any direction (10)
-    - insane - allow multiple monkeys (10)
-- allow user to add an animal which will move in various ways - give them the option to select the way of movement (10)
-- add 2 of your favorite other animals (other than chimp); create or find an image of similar size to the chimp; (10)
-- allow user to choose different colors for each animal when selected (5)
-- change the audio for a miss to say "missed" (5)
-- add background music, be sure to pause it when the user pauses the game (10) https://pythonprogramming.net/adding-sounds-music-pygame/
-https://freesound.org/browse/
-- add your own new feature to the game; more complex and engaging the more points (0 - 20)
-Zip ALL of the files needed to run your program and submit to Blackboard
-Also submit a separate readme.md file (create in github) that explains each of the new features you've added from the list above ALSO INCLUDE THE NUMBER OF POINTS FOR EACH ITEM
-BE SURE YOU DO NOT USE ANY ABSOLUTE FILE REFERENCES; all file references must be relative. Google this to learn more. 
-"""
 #Import Modules
 import os, pygame
 from pygame.locals import *
